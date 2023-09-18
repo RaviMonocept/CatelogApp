527d1ce9f34026fb035654db88cf20fd6bacdc83
Ravi Kumar <ravik@monocept.com>
2023-09-18 15:25:15 +0530

    Day21

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final days = 38;
+    final String name = "Ravi Kumar";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CataLog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome $name to $days days of flutter"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
