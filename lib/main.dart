2223d4a76e688b32504ca35f39e19cbdaf3e0f8c
RaviMonocept <ravik@monocept.com>
2023-09-21 12:09:39 +0530

    Day4Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/home_page.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,15 +15,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // home: HomePage(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
-        // "/": (context) => HomePage(),
+        "/": (context) => HomePage(),
         "/home": (context) => HomePage(),
-        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
       },
     );
   }
